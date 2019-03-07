ebe0fbd35631cec9192ba33c8ea8d572857919a9
yangshengkai <yangshengkai@chuchujie.com>
2019-03-07 11:22:51 +0800

    fiz

--- Algorithms/剑指Offer/kmp.php
+++ Algorithms/剑指Offer/kmp.php
@@ -8,8 +8,8 @@
  * @param $str2  模式串
  */
 
-$str1 = 'asfsdahfihieiocadasdesdnfhfafwe';
-$str2 = 'eiocadasde';
+$str1 = "aaabaaabbbabaa";
+$str2 = 'baaabbb';
 $a = kmp($str1, $str2);
 var_dump($a);
 function kmp($str1, $str2) {
@@ -27,7 +27,7 @@ function kmp($str1, $str2) {
             $j++;
         }
         if ($j == $length2) {
-            return $i - $length2 + 1;
+            return $i - $j + 1;
         }
     }
 
@@ -35,41 +35,21 @@ function kmp($str1, $str2) {
 }
 
 function getNext($str, $length) {
-    /**
-     * 极客时间-数据结构与算法提供的代码比较晦涩，下面一部分是比较容易理解的
-     * 可参考：https://www.zhihu.com/question/21923021
-     */
-//    $next = array();
-//    $next[0] = -1;
-//    $k = -1;
-//
-//    for ($i = 1; $i < $length; ++$i) {
-//        while ($k != -1 && $str[$k + 1] != $str[$i]) {
-//            $k = $next[$k];
-//        }
-//
-//        if ($str[$k + 1] == $str[$i]) {
-//            ++$k;
-//        }
-//        $next[$i] = $k;
-//
-//    }
-//    return $next;
     $next = array();
+    $next[0] = -1;
+    $k = -1;
 
-    $next[0] = -1;   //单纯的为了编程方便
-    $i = 0; //$i 和 $j索引其实都是为了遍历模式串
-    $j = -1;
-
-    while ($i < $length) {
-        if ($j == -1 || $str[$i] == $str[$j]) {  //注意 || 符号
-            $i++;
-            $j++;
-            $next[$i] = $j;  //索引$j同时还肩负着计数的作用
-        } else {
-            $j = $next[$j];  //为什么往回退这么多就是匹配的？
+    for ($i = 1; $i < $length; ++$i) {
+        while ($k != -1 && $str[$k + 1] != $str[$i]) {
+            $k = $next[$k];
         }
-    }
 
+        if ($str[$k + 1] == $str[$i]) {
+            ++$k;
+        }
+        $next[$i] = $k;
+
+    }
     return $next;
 }
+
